8d176bf3e90f9d020a66e992553f4bf558e36189
Hugovalpo <lillo.hugo@gmail.com>
2023-09-29 05:01:18 +0200

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Contact.module.css";
+import SectionTitle from "./SectionTitle";
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <section id="contact" className={styles.container}>
+      <SectionTitle title="Contact Me" titleNo="03." />
+      <h2 className={styles.subtitle}>Get in touch</h2>
+      <a href="mailto:lillo.hugo@gmail.com">
+        <button className={styles.button}>Send Mail</button>
+      </a>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
